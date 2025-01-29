166b90c2a5a6abf1efd77352165f9a4b7503fdb4
Antonin <antonin.maubert@edu.esiee-it.fr>
2025-01-29 21:22:06 +0100

    Finish updateStock

--- src/MainPharmacy.java
+++ src/MainPharmacy.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class MainPharmacy {
     public static void main(String[] args) {
         List<Product> legendes = new ArrayList<Product>();
-        List<Product>listProductCommande = new ArrayList<>();
+        List<Product> listProductCommande = new ArrayList<>();
         Bandage bd = new Bandage("bdd", 23.3F, 4, "Lebandant");
         Bandage bd1 = new Bandage("bdd2", 3.3F, 34, "Lebandant");
         Bandage bd2 = new Bandage("bdd1", 2.3F, 455, "Lebandant");
@@ -32,46 +32,52 @@ public class MainPharmacy {
                                 System.out.println("Enter the quantity: ");
                                 try {
                                     int inttmp = in.nextInt();
-                                    order.listProducts.add(new Bandage(tmp, p.getPrice(), inttmp, p.getCategory()));
-                                    if (p.getQuantity() < 5){
-                                        System.out.println("The product is lower than 5!");
+                                    if (inttmp > 0 && inttmp <= p.getQuantity()) {
+
+                                        order.listProducts.add(new Bandage(tmp, p.getPrice(), inttmp, p.getCategory()));
+                                        if (p.getQuantity() < 5) {
+                                            System.out.println("The product is lower than 5!");
+                                        }
+                                        System.out.println("Successfully bought!");
+                                        break;
+                                    } else {
+                                        System.out.println("You cant buy the quantity you asked !");
                                     }
-                                    System.out.println("Successfully bought!");
-                                    break;
                                 } catch (Exception _) {
                                 }
                             }
                             break;
+                        } else {
+                            System.out.println("Désolé le produit n'existe pas !");
                         }
                     }
 
-                        String st = in.nextLine();
-                        boolean flag = false;
-                        while (!flag) {
-                            System.out.println("Do you want to still want to buy some products?");
-                            st = in.nextLine();
-                            if (st.equalsIgnoreCase("no")) {
-                                System.out.println("See you soon!");
-                                for (Product p : order.listProducts) {
-                                    for (Product p2 : legendes) {
-                                        if (p.getName().equalsIgnoreCase(p2.getName())) {
-                                            p2.setQuantity(p2.getQuantity() - p.getQuantity());
-                                            System.out.println("Il reste actuellement :" + p2.quantity + p2.getName());
-                                        }
-                                        flag = true;
-                                    }
-                                }
-                            } else if (!st.equalsIgnoreCase("yes")) {
-                                System.out.println("Answer by yes or no.");
-                            }
-                            else {
-                                flag = true;
-                            }
-                        }
+                    String st = in.nextLine();
+                    boolean flag = false;
+                    while (!flag) {
+                        System.out.println("Do you want to still want to buy some products?");
+                        st = in.nextLine();
                         if (st.equalsIgnoreCase("no")) {
-                            wanttobuy = false;
-                            break;
+                            System.out.println("See you soon!");
+                            for (Product p : order.listProducts) {
+                                for (Product p2 : legendes) {
+                                    if (p.getName().equalsIgnoreCase(p2.getName())) {
+                                        p2.setQuantity(p2.getQuantity() - p.getQuantity());
+                                        System.out.println("Il reste actuellement :" + p2.quantity + p2.getName());
+                                    }
+                                    flag = true;
+                                }
+                            }
+                        } else if (!st.equalsIgnoreCase("yes")) {
+                            System.out.println("Answer by yes or no.");
+                        } else {
+                            flag = true;
                         }
+                    }
+                    if (st.equalsIgnoreCase("no")) {
+                        wanttobuy = false;
+                        break;
+                    }
 
                 }
 
